2474473cd581ba73d104935aa43b3c806d823c2d
Juan Carlos RIncon <juan.rincon@fislatecnologia.com>
2022-10-29 07:19:42 -0500

    Implementación de las nuevas pantallas.

---
+++ Controllers/WikiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UstaLab.Controllers
+{
+    public class WikiController : Controller
+    {
+        // GET: Wiki
+        public ActionResult PruebaRotorBloqueado()
+        {
+            return View();
+        }
+
+        public ActionResult PruebaVacio()
+        {
+            return View();
+        }
+    }
+}
